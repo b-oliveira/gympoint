965eccf29f223caf2f297bfd8b5ec22583846c0b
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-10-29 13:01:45 -0300

    Implementa autenticação JWT

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,8 @@
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
+
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
@@ -14,9 +18,14 @@ class SessionController {
     const { id, name } = user;
 
     return res.status(200).json({
-      id,
-      name,
-      email,
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     });
   }
 }
