48c29713b0301e0fcfe7e4b365b34f1672b58ac2
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-11-03 21:03:48 -0300

    Cria migration de estudante

---
+++ src/database/migrations/20191102163555-create-students.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      birth_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      weight: {
+        type: Sequelize.FLOAT(3, 2),
+        allowNull: false,
+      },
+      height: {
+        type: Sequelize.FLOAT(3, 2),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};
