e7676dfcb57ac778bc0f5846c2fa52f42c695586
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-11-04 12:29:42 -0300

    Validado dados de entrada no cadastro de estudante

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
+
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      birth_date: Yup.date().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: 'Validation fails!' });
+
     const { id, name, email, weight, height, age } = await Student.create(
       req.body
     );
