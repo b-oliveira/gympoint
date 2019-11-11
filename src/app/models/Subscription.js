a178ed0ade93bfa7827a3a42f9cb72ad3ec62329
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-11-10 21:17:38 -0300

    Cria model de inscrição

---
+++ src/app/models/Subscription.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Subscription extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        start_date: Sequelize.DATEONLY,
+        end_date: Sequelize.DATEONLY,
+        price: Sequelize.DOUBLE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Student, { foreignKey: 'student_id' });
+    this.belongsTo(models.Plan, { foreignKey: 'plan_id' });
+  }
+}
+
+export default Subscription;
