3a3720a92c82ba40bd16bcd5b81daeb9b29cac34
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-11-10 21:17:02 -0300

    Cria migration de inscrição

---
+++ src/database/migrations/20191110191319-create-subscriptions.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('subscriptions', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'plans', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      start_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('subscriptions');
+  },
+};
