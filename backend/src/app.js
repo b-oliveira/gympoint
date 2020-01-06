b4737603cb64e89e97dd4f280fa597c6afa93e94
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-01-05 22:05:40 -0300

    Configura sentry para tratamento de exceções

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
+import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -9,17 +13,30 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
